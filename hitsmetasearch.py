9aee0531161e1e35184bdec2d9f7abe9d8ea1f10
PLR <plredmond@gmail.com>
2012-07-08 18:35:52 -0400

    (not working) changed CLI argument order; minor changes and bugfixes

--- hitsmetasearch.py
+++ hitsmetasearch.py
@@ -13,13 +13,17 @@ import numpy
 import lib.trec
 
 
+##############################################################################
+## Utilities
+
+
 def mk_nodes(allruns):
     '''Return (set, set) of (sysids, docids).'''
-    doc_field = lib.trec.DOC
+    docfield = lib.trec.DOC
     docs = set()
     for arr in allruns.itervalues():
-        docs |= set(arr[doc_field])
-    return set(graph.keys()), docs
+        docs |= set(arr[docfield])
+    return set(allruns.keys()), docs
 
 
 def mk_edges(allruns, systup, doctup):
@@ -28,17 +32,25 @@ def mk_edges(allruns, systup, doctup):
     Return a numpy.array shaped (# sys, # docs) containing booleans.
 
     '''
-    doc_field = lib.trec.DOC
+    docfield = lib.trec.DOC
     edges = numpy.zeros([len(systup),len(doctup)], dtype=numpy.bool)
     docindexes = {docid:i for i, docid in enumerate(doctup)}
     for sys_i, sysid in enumerate(systup):
-            sysdocs = allruns[sisid][doc_field]
+            sysdocs = allruns[sysid][docfield]
             for docid in sysdocs:
                 doc_i = docindexes[docid]
                 edges[sys_i][doc_i] = True
             sysdocs = None
     del docindexes
     return edges
+
+
+class TestHITS(unittest.TestCase):
+    def setUp(self):
+        self.graph = {
+            'h1':assoc(zip('abcd', itertools.repeat(0))),
+            'h2':assoc(zip('ace', itertools.repeat(0))),
+        }
 
 
 ##############################################################################
@@ -49,14 +61,14 @@ if __name__ == '__main__':
     unittest.main()
     # args
     try:
-        queryno = int(sys.argv[1])
-        npzpath = sys.argv[2]
+        npzpath = sys.argv[1]
+        queryno = int(sys.argv[2])
         assert os.path.isdir(npzpath)
     except:
-        print 'USAGE: python {} INT PATH'.format(__file__)
+        print 'USAGE: python {} PATH INT'.format(__file__)
         print
-        print 'INT  - query number'
         print 'PATH - directory with *.npz files produced by compress.py'
+        print 'INT  - query number'
         exit()
     # data
     runs = lib.trec.load_comp_system_dir(npzpath, queryno)
@@ -64,14 +76,13 @@ if __name__ == '__main__':
         print 'No runs were loaded.'
         exit()
     # graph nodes
-    sysset, docset = mk_nodes(runs)
-    systup = tuple(sorted(syss))
-    doctup = tuple(sorted(docs))
-    del sysset
-    del docset
+    systup, doctup = (tuple(sorted(s)) for s in mk_nodes(runs))
     # graph edges
     sys_outlinks = mk_edges(runs, systup, doctup)
-    doc_inlinks = sys_outlinks.transpose()
+    x = doc_inlinks
+    print x.dtype, x.shape
+
+    exit()
     # perform analysis
     print len(authtup), 'authorities;', len(hubtup), 'hubs'
     k=[3]
