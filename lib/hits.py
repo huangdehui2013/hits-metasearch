439c5500a56809026a59fb9ad13fac67e3e8f1ad
PLR <plredmond@gmail.com>
2012-07-09 13:24:06 -0400

    (working) completed writing hits_update and hits; one test for hits_update

--- lib/hits.py
+++ lib/hits.py
@@ -2,71 +2,88 @@
 
 # stdlib
 import unittest
-import itertools
 # 3rd party
 import numpy
 
 
-def hits_update(a_old, h_old, h_outlinks, data):
-    # auths [x <- I()]
-    # hubs  [y <- O()]
-    # an auth score is the sum of the scores of the hubs which point to it
-    # a hub score is the sum of the scores of the auths to which it points
+def hits_update(a_old, h_old, a_inlinks, h_outlinks, data=None):
+    '''Update authority and hub scores.
+
+    1darr<num> 1darr<num> 2darr<bool> any --> 1darr<num> 1darr<num>
+
+    '''
     assert len(h_old) == h_outlinks.shape[0]
     assert len(a_old) == h_outlinks.shape[1]
-    a = numpy.fromiter((h_old[m].sum() for m in h_outlinks.transpose()),
+    # an auth score is the sum of the scores of the hubs which point to it
+    # a hub score is the sum of the scores of the auths to which it points
+    a = numpy.fromiter((h_old[m].sum() for m in a_inlinks),
         dtype=a_old.dtype, count=a_old.shape[0])
     h = numpy.fromiter((a_old[m].sum() for m in h_outlinks),
-        dtype=h_old.dtype, count=h_old.shape[1])
+        dtype=h_old.dtype, count=h_old.shape[0])
     return a, h
 
 
-def hits(a_inlinks, h_outlinks, update_fn, stopping_fn, data):
+def hits(h_outlinks, stopping_fn, update_fn=hits_update, sqrnorm=False,
+        data=None):
     '''Calculate hub and authority scores; end when stopping_fn returns true.
 
-    update_fn:
-        [1darr 1darr 2darr 2darr graph --> (assoc, assoc)]
-        [old_auths old_hubs auth_inlinks hub_outlinks --> (cur_auths, cur_hubs)]
+    h_outlinks:
+        2darr<bool>
+        An adjacency matrix indexed [hub][auth] with True for edges.
     stopping_fn:
-            True when the algorithm should stop, false otherwise
-        [int 1darr 1darr --> bool]
+        [int 1darr<num> 1darr<num> --> bool]
         [iteration_number cur_auths cur_hubs --> converged]
+        The algorithm will continue while this function returns True.
+    update_fn:
+        [1darr<num> 1darr<num> 2darr<bool> data --> (1darr<num>, 1darr<num>)]
+        [old_auths old_hubs hub_outlinks data --> (cur_auths, cur_hubs)]
+        Executed each iteration to update hub and authority scores.
+    sqrnorm:
+        bool
+        Should we normalize the square of the weights after each update?
+        (We normalize the raw weights when this is False.)
+    data:
+        Auxiliary data passed directly into update_fn.
 
     return:
-        (1darr, 1darr)
+        (1darr<num>, 1darr<num>)
         (final_auths, final_hubs)
 
     '''
-    # report sizes
+    a_inlinks = h_outlinks.transpose()
     # initialize scores to 1
-    a = numpy.ones(len(auttup), dtype=numpy.float64)
-    h = numpy.ones(len(hubtup), dtype=numpy.float64)
+    a = numpy.ones(h_outlinks.shape[1], dtype=numpy.float64)
+    h = numpy.ones(h_outlinks.shape[0], dtype=numpy.float64)
     # iteratively update scores
-    for i in itertools.count(1):
+    i = 1
+    while stopping_fn(i, a, h):
         print 'Iteration', i
-        # update i-1 to i
-        a, h = update_fn(a, h, a_inlinks, h_outlinks, graph)
+        # update
+        a, h = update_fn(a, h, a_inlinks, h_outlinks, data)
         # normalize
-        # a **= 2
-        # h **= 2
+        if sqrnorm:
+            a **= 2
+            h **= 2
         a /= a.sum()
         h /= h.sum()
-        # check stopping condition
-        if stopping_fn(i_math, a, h):
-            break
+        # count
+        i += 1
     return a, h
 
 
 class TestHITS(unittest.TestCase):
     def setUp(self):
-        self.oldauths = assoc([('a',3), ('b',1), ('c',4), ('d',3), ('e',5)])
-        self.oldhubs  = assoc([('h1',3), ('h2',7)])
-        self.newauths = assoc([('a',10), ('b',3), ('c',10), ('d',3), ('e',7)])
-        self.newhubs  = assoc([('h1',11), ('h2',13)])
+        self.a_old = numpy.array([3, 1, 4, 3, 5])
+        self.h_old = numpy.array([3, 7])
+        self.h_links = numpy.array([[1, 1, 1, 1, 0],
+                                    [1, 0, 1, 0, 1]], dtype=numpy.bool_)
+        self.a_new = numpy.array([10, 3, 10, 3, 7])
+        self.h_new = numpy.array([11, 12])
     def test__hits_update(self):
-        a, h = hits_update(self.graph, self.oldauths, self.oldhubs)
-        self.assertEqual(a, self.newauths)
-        self.assertEqual(h, self.newhubs)
+        a, h = hits_update(self.a_old, self.h_old,
+            self.h_links.transpose(), self.h_links)
+        self.assertTrue((a == self.a_new).all())
+        self.assertTrue((h == self.h_new).all())
 
 
 if __name__ == '__main__':
