dd9f9e0608fbb0b1f0891b85d3f8d3a06c4d6fec
PLR <plredmond@gmail.com>
2012-09-27 07:43:59 -0400

    reordered trials so bugs will surface more quickly

--- runtrials.sh
+++ runtrials.sh
@@ -2,17 +2,14 @@
 
 mkdir trials 2> /dev/null
 
-# edges equal to 1
-./hitsmetasearch.py data-trec8/comp/ 0   const > trials/hm0xconst
-./hitsmetasearch.py data-trec8/comp/ 10  const > trials/hm10xconst
-./hitsmetasearch.py data-trec8/comp/ 100 const > trials/hm100xconst
-
-# linear edge decay
+./hitsmetasearch.py data-trec8/comp/ 0   const  > trials/hm0xconst
 ./hitsmetasearch.py data-trec8/comp/ 0   linear > trials/hm0xlinear
-./hitsmetasearch.py data-trec8/comp/ 10  linear > trials/hm10xlinear
-./hitsmetasearch.py data-trec8/comp/ 100 linear > trials/hm100xlinear
-
-# nonlinear edge decay
 ./hitsmetasearch.py data-trec8/comp/ 0   negexp > trials/hm0xnegexp
+
+./hitsmetasearch.py data-trec8/comp/ 10  const  > trials/hm10xconst
+./hitsmetasearch.py data-trec8/comp/ 10  linear > trials/hm10xlinear
 ./hitsmetasearch.py data-trec8/comp/ 10  negexp > trials/hm10xnegexp
+
+./hitsmetasearch.py data-trec8/comp/ 100 const  > trials/hm100xconst
+./hitsmetasearch.py data-trec8/comp/ 100 linear > trials/hm100xlinear
 ./hitsmetasearch.py data-trec8/comp/ 100 negexp > trials/hm100xnegexp
