d379a798e6250e23f65ea546961f34a8dd79153b
PLR <plredmond@gmail.com>
2012-09-27 07:55:09 -0400

    changed order to negexp, linear, const for debugging faster

--- runtrials.sh
+++ runtrials.sh
@@ -2,14 +2,14 @@
 
 mkdir trials 2> /dev/null
 
-./hitsmetasearch.py data-trec8/comp/ 0   const  > trials/hm0xconst
-./hitsmetasearch.py data-trec8/comp/ 0   linear > trials/hm0xlinear
 ./hitsmetasearch.py data-trec8/comp/ 0   negexp > trials/hm0xnegexp
+./hitsmetasearch.py data-trec8/comp/ 0   linear > trials/hm0xlinear
+./hitsmetasearch.py data-trec8/comp/ 0   const  > trials/hm0xconst
 
-./hitsmetasearch.py data-trec8/comp/ 10  const  > trials/hm10xconst
-./hitsmetasearch.py data-trec8/comp/ 10  linear > trials/hm10xlinear
 ./hitsmetasearch.py data-trec8/comp/ 10  negexp > trials/hm10xnegexp
+./hitsmetasearch.py data-trec8/comp/ 10  linear > trials/hm10xlinear
+./hitsmetasearch.py data-trec8/comp/ 10  const  > trials/hm10xconst
 
-./hitsmetasearch.py data-trec8/comp/ 100 const  > trials/hm100xconst
-./hitsmetasearch.py data-trec8/comp/ 100 linear > trials/hm100xlinear
 ./hitsmetasearch.py data-trec8/comp/ 100 negexp > trials/hm100xnegexp
+./hitsmetasearch.py data-trec8/comp/ 100 linear > trials/hm100xlinear
+./hitsmetasearch.py data-trec8/comp/ 100 const  > trials/hm100xconst
