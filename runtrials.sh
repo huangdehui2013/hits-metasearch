3a31cd9f22e4981611ced1c30264cc48bda963a7
PLR <plredmond@gmail.com>
2012-09-26 22:49:21 -0400

    added paragraphs for each edge decay method

--- runtrials.sh
+++ runtrials.sh
@@ -1,9 +1,18 @@
 #!/bin/sh
 
-./hitsmetasearch.py data-trec8/comp/ 0   > hm0xones
-./hitsmetasearch.py data-trec8/comp/ 10  > hm10xones
-./hitsmetasearch.py data-trec8/comp/ 100 > hm100xones
+mkdir trials 2> /dev/null
 
-# linear func...?
+# edges equal to 1
+./hitsmetasearch.py data-trec8/comp/ 0   const > trials/hm0xconst
+./hitsmetasearch.py data-trec8/comp/ 10  const > trials/hm10xconst
+./hitsmetasearch.py data-trec8/comp/ 100 const > trials/hm100xconst
 
-# nonlinear func??
+# linear edge decay
+./hitsmetasearch.py data-trec8/comp/ 0   linear > trials/hm0xlinear
+./hitsmetasearch.py data-trec8/comp/ 10  linear > trials/hm10xlinear
+./hitsmetasearch.py data-trec8/comp/ 100 linear > trials/hm100xlinear
+
+# nonlinear edge decay
+./hitsmetasearch.py data-trec8/comp/ 0   negexp > trials/hm0xnegexp
+./hitsmetasearch.py data-trec8/comp/ 10  negexp > trials/hm10xnegexp
+./hitsmetasearch.py data-trec8/comp/ 100 negexp > trials/hm100xnegexp
